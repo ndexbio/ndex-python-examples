8b0efdc591f4d34ae9367dee0856b421a2de85c9
Dexter Pratt <dexterpratt.bio@gmail.com>
2016-09-14 16:52:51 -0600

    reorganization to temporarily cope with notebook importing

--- demo_notebooks/causal_paths/causal_test_clean.py
+++ demo_notebooks/causal_paths/causal_test_clean.py
@@ -13,7 +13,7 @@ network_id='50e3dff7-133e-11e6-a039-06603eb7f303'
 G = NdexGraph(server=ndex_server, uuid=network_id, username=rm_username, password=rm_password)
 
 # Compute the source-target network
-P1 = cu.get_source_target_network(G, ['MAP2K1'], ['MMP9'], "MAP2K1 to MMP9 - again", npaths=20)
+P1 = cu.get_source_target_network(G, ['EGFR'], ['MAP2K1, MAP2K2'], "EGFR to MAP2K1, MAP2K2", npaths=20)
 
 # Apply a layout
 toolbox.apply_source_target_layout(P1)
@@ -24,6 +24,19 @@ toolbox.apply_template(P1, template_id)
 
 # Save to NDEx in the drh account
 P1.upload_to(ndex_server, 'drh', 'drh')
+
+# Compute the source-target network
+P2 = cu.get_source_target_network(G, ['MAP2K1'], ['MMP9'], "MAP2K1 to MMP9", npaths=20)
+
+# Apply a layout
+toolbox.apply_source_target_layout(P2)
+
+# Apply a cytoscape style from a template network
+template_id = '4f53171c-600f-11e6-b0a6-06603eb7f303'
+toolbox.apply_template(P2, template_id)
+
+# Save to NDEx in the drh account
+P2.upload_to(ndex_server, 'drh', 'drh')
 
 
 

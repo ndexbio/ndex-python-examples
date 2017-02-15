56ff9eb16c44cac61a0f5ce0f829e81f8600f8c6
Aaron Gary <agary@ucsd.edu>
2017-02-14 17:12:50 -0800

    added DeRibosylation

--- causal_paths/src/path_scoring.py
+++ causal_paths/src/path_scoring.py
@@ -159,7 +159,8 @@ class EdgeRanking:
                 "Acetylation",
                 "Deacetylation",
                 "Sumoylation",
-                "Ribosylation"
+                "Ribosylation",
+                "Deribosylation"
             ],
             EdgeEnum.proteins_catalysis_lsmr: [  # 6
                 "catalysis-precedes"
